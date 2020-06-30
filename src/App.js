10771ba8cc106911f3fac033235faa2680532778
Ashish Gautam <ashishgxzc@gmail.com>
2020-06-30 22:00:56 +0530

    added components in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Banner from './components/Banner';
+import InformationCharts from './components/InformationCharts';
+import Footer from './components/Footer';
+
+import './App.scss';
 
 function App() {
   return (
     <div>
-
+        <Banner />
+        <InformationCharts />
+        <Footer />
     </div>
   );
 }
